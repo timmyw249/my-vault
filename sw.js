e4b94cdbbcdb181b9ea86c509054af01e6d16f3b
timmyw249 <timmyw249@yahoo.com>
2026-09-25 20:26:01 -0500

    Refactor service worker caching logic

--- sw.js
+++ sw.js
@@ -4,12 +4,10 @@ const ASSETS = [
 './index.html',
 './styles.css',
 './app.js'
-]; 
-
+];
 self.addEventListener('install', (e) => {
 e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-}); 
-
+});
 self.addEventListener('fetch', (e) => {
 e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
 });
